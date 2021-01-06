07919ae50e1a67942545a46cff2557a5b1005e8d
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-01-06 14:18:25 +0600

    BT-334

---
+++ src/aws_lambda_functions/get_presigned_url_to_download_file/lambda_function.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import json
+import boto3
+
+# Configure the logging tool in the AWS Lambda function.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+# Initialize constants with parameters to configure.
+BUCKET_NAME = os.environ["BUCKET_NAME"]
+
+
+def lambda_handler(event, context):
+    """
+    :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
+    :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
+    """
+    # Use AWS Lambda proxy integration.
+    try:
+        query_string_parameters = event["queryStringParameters"]
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Define all necessary parameters to generate the presigned URL.
+    try:
+        key_name = query_string_parameters["key_name"]
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Create a low-level service client by name using the default session.
+    s3_client = boto3.client("s3")
+
+    # Generate the presigned URL for the S3 object.
+    try:
+        presigned_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={
+                "Bucket": BUCKET_NAME,
+                "Key": key_name
+            },
+            ExpiresIn=3600
+        )
+    except Exception as error:
+        logging.error(error)
+        return None
+
+    # Return the status code 200.
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "presignedUrl": presigned_url
+        })
+    }
