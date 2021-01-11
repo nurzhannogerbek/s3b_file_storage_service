74806645e4bade4f78067b00f0299da2ff24f4de
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-01-11 14:29:32 +0600

    BT-334

---
+++ src/aws_lambda_functions/automatically_resize_and_crop_image/lambda_function.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import boto3
+from PIL import Image
+from io import BytesIO
+
+# Configure the logging tool in the AWS Lambda function.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+# Initialize constants with parameters to configure.
+S3_ACCESS_KEY_ID = os.environ["S3_ACCESS_KEY_ID"]
+S3_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
+S3_BUCKET = os.environ["S3_BUCKET"]
+
+# Create a low-level service client by name using the default session.
+s3_client = boto3.client("s3", aws_access_key_id=S3_ACCESS_KEY_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY)
+
+
+def lambda_handler(event, context):
+    """
+    :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
+    :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
+    """
+    # Return nothing.
+    return None
--- src/aws_lambda_functions/get_presigned_url_to_upload_file/lambda_function.py
+++ src/aws_lambda_functions/get_presigned_url_to_upload_file/lambda_function.py
@@ -2,6 +2,7 @@ import logging
 import os
 import json
 import boto3
+from urllib.parse import unquote_plus
 
 # Configure the logging tool in the AWS Lambda function.
 logger = logging.getLogger(__name__)
@@ -10,6 +11,7 @@ logger.setLevel(logging.ERROR)
 # Initialize constants with parameters to configure.
 S3_ACCESS_KEY_ID = os.environ["S3_ACCESS_KEY_ID"]
 S3_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
+S3_DEFAULT_REGION = os.environ["S3_DEFAULT_REGION"]
 S3_BUCKET = os.environ["S3_BUCKET"]
 
 # Create a low-level service client by name using the default session.
@@ -21,16 +23,9 @@ def lambda_handler(event, context):
     :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
     :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
     """
-    # Use AWS Lambda proxy integration.
-    try:
-        query_string_parameters = event["queryStringParameters"]
-    except Exception as error:
-        logger.error(error)
-        raise Exception(error)
-
     # Define all necessary parameters to generate the presigned URL.
     try:
-        s3_key = query_string_parameters["s3_key"]
+        s3_key = unquote_plus(event["queryStringParameters"]["s3_key"])
     except Exception as error:
         logger.error(error)
         raise Exception(error)
@@ -42,7 +37,7 @@ def lambda_handler(event, context):
             Key=s3_key,
             Fields=None,
             Conditions=None,
-            ExpiresIn=3600
+            ExpiresIn=60
         )
     except Exception as error:
         logging.error(error)
@@ -53,6 +48,6 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "data": presigned_url,
-            "url": "https://{0}.s3.amazonaws.com/{1}".format(S3_BUCKET, s3_key)
+            "url": "https://{0}.s3.{1}.amazonaws.com/{2}".format(S3_BUCKET, S3_DEFAULT_REGION, s3_key)
         })
     }
