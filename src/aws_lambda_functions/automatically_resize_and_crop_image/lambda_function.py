5df57ad892c3ed1035a8e4d5fdc00d79dd48b0dd
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-01-13 18:55:16 +0600

    BT-334

--- src/aws_lambda_functions/automatically_resize_and_crop_image/lambda_function.py
+++ src/aws_lambda_functions/automatically_resize_and_crop_image/lambda_function.py
@@ -1,8 +1,9 @@
 import logging
 import os
 import boto3
-from io import BytesIO
+from urllib.parse import unquote_plus
 import cv2
+import numpy
 
 # Configure the logging tool in the AWS Lambda function.
 logger = logging.getLogger(__name__)
@@ -14,8 +15,8 @@ S3_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
 S3_DEFAULT_REGION = os.environ["S3_DEFAULT_REGION"]
 FILE_STORAGE_NAME = os.environ["FILE_STORAGE_NAME"]
 
-# Create a low-level service client by name using the default session.
-s3_client = boto3.client("s3", aws_access_key_id=S3_ACCESS_KEY_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY)
+# Create the resource service client.
+s3_resource = boto3.resource("s3", aws_access_key_id=S3_ACCESS_KEY_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY)
 
 
 def lambda_handler(event, context):
@@ -35,28 +36,31 @@ def lambda_handler(event, context):
     for key in event.get("Records"):
         # Define the value of the S3 bucket object key.
         try:
-            original_s3_object_key = key["s3"]["object"]["key"]
+            original_s3_object_key = unquote_plus(key["s3"]["object"]["key"])
         except KeyError as error:
             logger.error(error)
             raise Exception(error)
 
         # Grab the original source file.
-        original_s3_object = s3_client.Object(bucket_name=FILE_STORAGE_NAME, key=original_s3_object_key)
+        original_s3_object = s3_resource.Object(bucket_name=FILE_STORAGE_NAME, key=original_s3_object_key)
         original_s3_object_body = original_s3_object.get()["Body"].read()
 
         # Read the image file.
-        original_image = cv2.imread(BytesIO(original_s3_object_body))
+        original_image = cv2.imdecode(numpy.asarray(bytearray(original_s3_object_body)), cv2.IMREAD_COLOR)
 
-        # Define the original image file's size.
+        # Define the size of the original image file.
         original_image_width, original_image_height = original_image.shape[1], original_image.shape[0]
+
+        # Define the extension of the original image file.
+        original_image_file_extension = os.path.splitext(original_s3_object_key)[1].lower()
 
         for parameter in sizes:
             # Create the unique S3 bucket object key for specific size.
             original_s3_object_key_parts = original_s3_object_key.rsplit('/', 1)
-            new_s3_object_key = "{0}/{1}x{2}/{3}".format(
-                original_s3_object_key_parts[0],
-                parameter["width"],
-                parameter["height"],
+            image_size = "{0}x{1}".format(parameter["width"], parameter["height"])
+            new_s3_object_key = "{0}/{1}/{2}".format(
+                original_s3_object_key_parts[0].replace("users/", "users_cropped_images/"),
+                image_size,
                 original_s3_object_key_parts[1]
             )
 
@@ -69,16 +73,13 @@ def lambda_handler(event, context):
                 new_image_height = parameter["height"]
             else:
                 new_image_height = original_image_height
-            mid_x, mid_y = int(original_image_width / 2), int(original_image_height / 2)
-            cw2, ch2 = int(new_image_width / 2), int(new_image_height / 2)
-            new_image = original_image[mid_y - ch2:mid_y + ch2, mid_x - cw2:mid_x + cw2]
-            buffer = BytesIO()
-            new_image.save(buffer, format)
-            buffer.seek(0)
+            x, y = int(original_image_width/2), int(original_image_height/2)
+            width, height = int(new_image_width/2), int(new_image_height/2)
+            new_image = original_image[y-height:y+height, x-width:x+width]
 
             # Upload the new image file.
-            new_s3_object_body = s3_client.Object(bucket_name=FILE_STORAGE_NAME, key=new_s3_object_key)
-            new_s3_object_body.put(Body=buffer)
+            new_s3_object = s3_resource.Object(bucket_name=FILE_STORAGE_NAME, key=new_s3_object_key)
+            new_s3_object.put(Body=cv2.imencode(original_image_file_extension, new_image)[1].tobytes())
 
     # Return nothing.
     return None
--- src/aws_lambda_functions/resize_and_crop_image/lambda_function.py
+++ src/aws_lambda_functions/resize_and_crop_image/lambda_function.py
@@ -1,7 +1,10 @@
 import logging
 import os
 import boto3
+import json
+from urllib.parse import unquote_plus
 import cv2
+import numpy
 
 # Configure the logging tool in the AWS Lambda function.
 logger = logging.getLogger(__name__)
@@ -13,8 +16,8 @@ S3_SECRET_ACCESS_KEY = os.environ["S3_SECRET_ACCESS_KEY"]
 S3_DEFAULT_REGION = os.environ["S3_DEFAULT_REGION"]
 FILE_STORAGE_NAME = os.environ["FILE_STORAGE_NAME"]
 
-# Create a low-level service client by name using the default session.
-s3_client = boto3.client("s3", aws_access_key_id=S3_ACCESS_KEY_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY)
+# Create the resource service client.
+s3_resource = boto3.resource("s3", aws_access_key_id=S3_ACCESS_KEY_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY)
 
 
 def lambda_handler(event, context):
@@ -22,5 +25,72 @@ def lambda_handler(event, context):
     :param event: The AWS Lambda function uses this parameter to pass in event data to the handler.
     :param context: The AWS Lambda function uses this parameter to provide runtime information to your handler.
     """
-    # Return nothing.
-    return None
+    # Make sure that all the necessary arguments for the AWS Lambda function are present.
+    try:
+        original_s3_object_key = unquote_plus(event["queryStringParameters"]["s3_key"])
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        body = json.loads(event["body"])
+    except Exception as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        sizes = body["sizes"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+    try:
+        coordinates = body["coordinates"]
+    except KeyError as error:
+        logger.error(error)
+        raise Exception(error)
+
+    # Grab the original source file.
+    original_s3_object = s3_resource.Object(bucket_name=FILE_STORAGE_NAME, key=original_s3_object_key)
+    original_s3_object_body = original_s3_object.get()["Body"].read()
+
+    # Read the image file.
+    original_image = cv2.imdecode(numpy.asarray(bytearray(original_s3_object_body)), cv2.IMREAD_COLOR)
+
+    # Define the extension of the original image file.
+    original_image_file_extension = os.path.splitext(original_s3_object_key)[1].lower()
+
+    # Define the empty list of urls.
+    urls = []
+
+    for parameter in sizes:
+        # Create the unique S3 bucket object key for specific size.
+        original_s3_object_key_parts = original_s3_object_key.rsplit('/', 1)
+        image_size = "{0}x{1}".format(parameter["width"], parameter["height"])
+        new_s3_object_key = "{0}/{1}/{2}".format(
+            original_s3_object_key_parts[0].replace("users/", "users_cropped_images/"),
+            image_size,
+            original_s3_object_key_parts[1]
+        )
+
+        # Crop the original image file. Format: image[start_y:end_y, start_x:end_x].
+        new_image = original_image[coordinates["y"]:parameter["height"], coordinates["x"]:parameter["width"]]
+
+        # Upload the new image file.
+        new_s3_object = s3_resource.Object(bucket_name=FILE_STORAGE_NAME, key=new_s3_object_key)
+        new_s3_object.put(Body=cv2.imencode(original_image_file_extension, new_image)[1].tobytes())
+
+        # Add the url address of the new image file to the array.
+        url = {
+            image_size: "https://{0}.s3.{1}.amazonaws.com/{2}".format(
+                FILE_STORAGE_NAME,
+                S3_DEFAULT_REGION,
+                new_s3_object_key
+            )
+        }
+        urls.append(url)
+
+    # Return the status code 200.
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "urls": urls
+        })
+    }
